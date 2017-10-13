0149b864ab5d4708e32140fb42859671ba44f426
Derek Owusu-Frimpong <deepsky_5@live.com>
2017-10-13 00:57:34 +0000

    add routes to registrations for omniauth callback

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,9 @@
 Rails.application.routes.draw do
 
-  devise_for :users, controllers: { confirmation: 'confirmations', registrations: "registrations" }
+  devise_for :users, controllers: { confirmation: 'confirmations', registrations: 'registrations',  omniauth_callbacks: 'omniauth_callbacks' }
   devise_scope :user do 
-    root 'welcome#index', to: "devise/registrations#new"
+    root 'welcome#index', to: 'devise/registrations#new'
   end
 
-  get "countdown/landing_page", to: "countdown#landing_page", as: :countdown
+  get 'countdown/landing_page', to: 'countdown#landing_page', as: :countdown
 end
